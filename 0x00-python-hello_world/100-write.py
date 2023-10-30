df3ec9b6b272d5011d168c8fbfd1fdd81de30b0b
EsraaGD <isra.hatim2@gmail.com>
2023-10-30 13:02:05 -0700

    Task 12

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,6 +2,6 @@
 
 import sys
 
-message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-sys.stderr.write(message + "\n")
+theart = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+sys.stderr.write(theart + "\n")
 sys.exit(1)
