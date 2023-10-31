1b392e17ed9baae8f01f83f7f13f79d95dddfddb
EsraaGD <isra.hatim2@gmail.com>
2023-10-31 14:23:54 -0700

    Task 14

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for av in range(122, 96, -1):
+    char = chr(av)
+    case = "lower" if av % 2 == 0 else "upper"
+    print(f"{char.upper() if case == 'lower' else char}", end="")
