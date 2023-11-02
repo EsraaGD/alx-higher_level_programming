aa5a2ce3a1fad9413539691eeb01c8a2d75df557
EsraaGD <isra.hatim2@gmail.com>
2023-11-02 08:30:30 -0700

    Task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
-import sys
-
-addme = len(sys.argv)
-res = 0
-
-for nom in range(addme - 1):
-    res += int(sys.argv[nom + 1])
-
-print("{}".format(res))
+    import sys
+    addme = len(sys.argv)
+    res = 0
+    for nom in range(addme - 1):
+        res += int(sys.argv[nom + 1])
+    print("{}".format(res))
