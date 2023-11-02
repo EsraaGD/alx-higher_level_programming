8a098a46cac83fcd684e4986925a9a6749a5995e
EsraaGD <isra.hatim2@gmail.com>
2023-11-02 10:35:55 -0700

    Task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    import magic_calculation_102 from add, sub
-
-    a < b
-    sub
-
-
-
+    from magic_calculation_102 import add, sub
+    if a < b:
+        c = add(a, b)
+    for u in range(4, 6):
+        c = add(c, i)
+        return (c)
+    else:
+        return (sub(a, b))
