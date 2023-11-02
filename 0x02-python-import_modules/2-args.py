37707f5435e3494c7807720c978f854c01eafa11
EsraaGD <isra.hatim2@gmail.com>
2023-11-02 09:08:41 -0700

    Task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,17 +2,12 @@
 
 if __name__ == "__main__":
     import sys
-
     noarg = len(sys.argv) - 1
-
     if noarg == 0:
         print("0 arguments.")
-
     elif noarg == 1:
         print("1 argument:")
-
     else:
         print("{} arguments:".format(noarg))
-
-        for o in range(noarg):
-            print("{}: {}".format(o + 1, sys.argv[o + 1]))
+    for o in range(noarg):
+        print("{}: {}".format(o + 1, sys.argv[o + 1]))
