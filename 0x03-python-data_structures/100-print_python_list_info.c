7a4b4d4fd105fa872318977666e7b21c428aaad0
EsraaGD <isra.hatim2@gmail.com>
2023-11-04 08:00:55 -0700

    Task 8/9

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,19 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+void print_python_list_info(PyObject *p) {
+    int size, alloc;
+    PyListObject *list = (PyListObject *)p;
+    PyObject *element;
+
+    size = Py_SIZE(p);
+    alloc = list->allocated;
+
+    printf("[*] Size of the Python List = %d\n", size);
+    printf("[*] Allocated = %d\n", alloc);
+
+    for (int i = 0; i < size; i++) {
+        element = PyList_GetItem(p, i);
+        printf("Element %d: %s\n", i, Py_TYPE(element)->tp_name);
+    }
+}
