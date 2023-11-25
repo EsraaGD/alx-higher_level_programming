5cc676b7382e9c9708b74842c36e5b49137e942b
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2023-11-25 17:25:15 +0300

    Task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+    """_summary_"""
+
+
+def print_square(size):
+    """Print square with size size using #
+
+    Args:
+        size (int): size of lenghth of square
+
+    Raises:
+        TypeError: if size is not an int
+        ValueError: if size is less than 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an interger")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
