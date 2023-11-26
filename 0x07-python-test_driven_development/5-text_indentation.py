a7fc56ed405ee605a7e372480d7e726564f0f77f
EsraaGD <isra.hatim2@gmail.com>
2023-11-26 09:01:53 -0800

    Task 5 done

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
-    """Define function"""
-    
-    
+
 def text_indentation(text):
-    """Print the text with 2 lines after '.', '?', ':'.
-
-    Args:
-        text (str): Input text to be formatted
-
-    Raises:
-        TypeError: If input text is not a string
-    """
+    """Indent text based on delimiters (?, :)"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
-    gonew_line = ['.', '?', ':']
-    formatted_text = ""
-    
-    for char in text:
-        formatted_text += char
-        if char in gonew_line:
-            formatted_text += '\n\n'
-            
-    for line in formatted_text.split('\n'):
-        print(line.strip())
+
+    for delimiter in "?:.":
+        text = (delimiter + "\n\n").join(
+            [sentence.strip(" ") for sentence in text.split(delimiter)])
+
+    print(text)
