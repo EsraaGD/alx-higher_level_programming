d799a6866510fa67ce982cfcda4c38d4ee8edb2e
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2023-11-28 00:18:20 +0300

    Task 2 last fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -83,6 +83,6 @@ class Rectangle:
         Returns:
             int: perimeter of rec
         """
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__height + self.__width)
