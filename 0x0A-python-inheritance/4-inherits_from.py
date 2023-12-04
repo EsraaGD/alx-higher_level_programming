b8331d16006e682adeefe8b3b2e4e8fb27a0a488
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2023-12-04 21:55:08 +0300

    Task4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Something
+"""
+
+
+def inherits_from(obj, a_class):
+    """_summary_
+
+    Args:
+        obj (_type_): _description_
+        a_class (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return issubclass(type(obj), a_class)
