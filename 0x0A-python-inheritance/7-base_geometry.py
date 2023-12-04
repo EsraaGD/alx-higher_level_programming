c46c905b48bd49fb798e5b7f12c21d2f9dd0078d
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2023-12-04 23:20:02 +0300

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""_summary_"""
+
+
+class BaseGeometry():
+    """empty"""
+
+    def area(self):
+        """Area not implemeted
+
+        Raises:
+            Exception: Area is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate value integer and is > 0
+
+        Args:
+            name (str): name of value
+            value: value to be validated
+
+        Raises:
+            TypeError: name must be integer
+            ValueError: name must be greater than 0
+        """
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
