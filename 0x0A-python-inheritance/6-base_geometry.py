bbd6e07005d4d62b416a1adafd8cea113322c164
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2023-12-04 22:46:25 +0300

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+
+def area(self):
+    """_summary_
+
+    Raises:
+        Exception: Area is not implemented
+    """
+    raise Exception("area() is not implemented")
