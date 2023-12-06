bcf7d94a30d5d95ae3a4d5149421fe848f1eea54
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2023-12-06 21:03:15 +0300

    Task 7 and 14

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list,
-and then save them to a file"""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
+
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
