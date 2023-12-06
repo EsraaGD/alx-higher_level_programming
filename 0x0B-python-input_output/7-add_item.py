c6c7d04b6b4196b443182c502e58c5c0c3efcbd3
EsraaGD <isra.hatim2@gmail.com>
2023-12-06 20:52:54 +0000

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,14 +4,15 @@
 Contains a script that adds all command line arguments to a Python list and
 saves the list as a JSON representation in a file named add_item.json.
 """
+
+
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 
 def add_items_to_list():
     """Adds all command line arguments to a Python list and saves it to a file."""
-    # Get the existing list from the file, or create an empty list if the file doesn't exist.
     try:
         existing_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
