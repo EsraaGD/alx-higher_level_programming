aaaf6a44f6c8690d2467c069ffdf036bf8f51b8a
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2023-12-08 20:37:18 +0300

    Tasks 19 and 20

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,59 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(10, 10)
+        else:
+            dummy = cls(10)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        filename = "{}.json".format(cls.__name__)
+        try:
+            with open(filename, 'r') as file:
+                json_string = file.read()
+                dic = cls.from_json_string(json_string)
+                inst = [cls.create(**dictionary) for dictionary in dic]
+                return inst
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        filename = "{}.csv".format(cls.__name__)
+
+        with open(filename, mode='w') as file:
+            writer = csv.writer(file)
+
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    writer.writerow(
+                        [obj.id, obj.width, obj.height, obj.x, obj.y])
+                elif cls.__name__ == "Square":
+                    writer.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        filename = "{}.csv".format(cls.__name__)
+        inst = []
+
+        try:
+            with open(filename, mode='r') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if cls.__name__ == "Rectangle":
+                        inst = cls(id=int(row[0]), width=int(row[1]), height=int(row[2]),
+                                   x=int(row[3]), y=int(row[4]))
+                    elif cls.__name__ == "Square":
+                        inst = cls(id=int(row[0]), size=int(row[1]),
+                                   x=int(row[2]), y=int(row[3]))
+                    inst.append(inst)
+        except FileNotFoundError:
+            pass
+
+        return inst
