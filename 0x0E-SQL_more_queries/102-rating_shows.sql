664a8daf36732f85a4d0d8d73b242a7c2ffbc72b
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-01-18 00:35:55 +0300

    T19 & T20

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,6 @@
 --lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT title, SUM(rating) as rating_sum
-FROM hbtn_0d_tvshows_rate
-GROUP BY title
-ORDER BY rating_sum DESC;
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
