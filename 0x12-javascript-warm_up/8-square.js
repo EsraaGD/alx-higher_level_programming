11ed8146dd09e30b5463b3ccac803a87bb33f34f
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-02-12 23:36:41 +0300

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const sq = parseInt(process.argv[2]);
+if (isNaN(sq)) {
+	console.log('Missing size');
+} else {
+	for (let i = 0; i < sq; i++) {
+		console.log('X'.repeat(sq));
+	}
+}
