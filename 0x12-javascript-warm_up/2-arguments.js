b4ea03b19e80796c2dbcc5aafa46c1e630315ceb
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-02-12 21:55:31 +0300

    All files + 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 if (process.argv[3]) {
     console.log('Arguments found');
-} else if (process.argv[2]) {
+}
+else if (process.argv[2]) {
     console.log('Argument found');
-} else {
+}
+else {
     console.log('No argument');
 }
