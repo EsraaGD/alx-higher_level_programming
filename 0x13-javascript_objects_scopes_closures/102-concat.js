33dde02ecf8fe41858549f7b18e44ab0c0c76931
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-02-14 20:06:05 +0300

    Task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,22 +1,5 @@
+#!/usr/bin/node
 const fs = require('fs');
-
-// Get file paths from command line arguments
-const [, , sourceFile1, sourceFile2, destinationFile] = process.argv;
-
-// Read contents of source files
-fs.readFile(sourceFile1, 'utf8', (err1, data1) => {
-  if (err1) throw err1;
-
-  fs.readFile(sourceFile2, 'utf8', (err2, data2) => {
-    if (err2) throw err2;
-
-    // Concatenate contents of both files
-    const concatenatedData = data1 + data2;
-
-    // Write concatenated data to the destination file
-    fs.writeFile(destinationFile, concatenatedData, err => {
-      if (err) throw err;
-      console.log('Files concatenated successfully!');
-    });
-  });
-});
+const first = fs.readFileSync(process.argv[2], 'utf8');
+const second = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], first + second);
