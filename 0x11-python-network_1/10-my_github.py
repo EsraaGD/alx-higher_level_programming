f98d569159c9d34b043d0a82e2a6b76945c249ef
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-01 22:28:10 +0300

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""takes your GitHub credentials & uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+def display_gitid(username, password):
+    """Use basic auth with Personal Access Token as password"""
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        print("User ID:", user_data["id"])
+    else:
+        print("Failed to fetch user ID.")
+        print("HTTP status code:", response.status_code)
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    display_gitid(username, password)
