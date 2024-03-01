ce55c6b02e2cd1d4b9ba6fab905e4fa21bb811d3
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-01 22:06:23 +0300

    7 8

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+""" sends a request to the URL
+& displays the body of the response (decoded in utf-8)."""
+import requests
+import sys
+
+
+def err2(url):
+    """if stat code is >= 400, print: Error code:
+    followed by the HTTP status code"""
+
+    response = requests.get(url)
+    if response.statcode >= 400:
+        print("Error code:", response.statcode)
+    else:
+        print(response.text)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    err2(url)
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+def searchuser(letter):
+    """sends a POST request to http://0.0.0.0:5000/search_user with
+    the letter as a parameter."""
+    url = 'http://0.0.0.0:5000/search_user'
+    payload = {'q': letter}
+    response = requests.post(url, data=payload)
+
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    searchuser(letter)
