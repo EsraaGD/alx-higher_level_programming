73554cd9675fe29e88ec4f698d094853da3c70cb
EsraaGD <isra.hatim2@gmail.com>
2024-02-29 16:32:18 -0800

    T3again

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept
-curl -s -I -X OPTIONS "$1" | grep -i allow | sed 's/Allow://' | tr -d '\r'
+curl -s -I -X OPTIONS "$1" | grep -i allow | awk '/^Allow:/ {gsub(/^Allow: /, ""); print}'
