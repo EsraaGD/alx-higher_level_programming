14b700f9b94423da8e6c4876e4618c721e16f80b
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-01 21:08:43 +0300

    3 4

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" sends a request to the URL
+& displays the body of the response (decoded in utf-8)."""
+import urllib.request
+import urllib.error
+import sys
+
+
+def err(url):
+    """manage urllib.error.HTTPError exceptions
+    & print: Error code: followed by the HTTP status code"""
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    err(url)
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+
+def fetch_stat_req():
+    """must use requests"""
+
+    url = ""
+    response = requests.get(url)
+    content = response.text
+    print("Body resonse:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+
+
+if __name__ == "__main__":
+    fetch_stat_req()
