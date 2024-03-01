78ccb9197f33098c566ff7400eefe5141e6b2646
EsraaGD <isra.hatim2@gmail.com>
2024-03-01 04:13:25 +0300

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,17 +10,11 @@ def find_peak(list_of_integers):
     - The peak integer found in the list.
     - None if the list is empty.
     """
-    if len(list_of_integers) == 0:
+    if not list_of_integers:
         return None
 
-    left = 0
-    right = len(list_of_integers) - 1
+    for i in range(len(list_of_integers) - 1):
+        if list_of_integers[i] > list_of_integers[i + 1]:
+            return list_of_integers[i]
 
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-
-    return list_of_integers[left]
+    return list_of_integers[-1]
