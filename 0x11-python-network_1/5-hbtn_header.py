7f7415321399b47bdd0a4829541b9c15b57e63b4
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-01 21:38:41 +0300

    5 6

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""sends a request to the URL
+   & displays the value of the X-Request-Id variable"""
+import requests
+import sys
+
+
+def display_xrid2(url):
+    """Display val of X-Request-Id"""
+    response = requests.get(url)
+    xrid2 = response.headers.get('X-Request-Id')
+    print(xrid2)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    display_xrid2(url)
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response"""
+import requests
+import sys
+
+
+def post_r2(url, email):
+    """Sends a POST req with email as a parameter
+    & displays the body of the response"""
+
+    payload = {'email': email}
+    resp = requests.post(url, data=payload)
+    print(resp.text)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    post_r2(url, email)
