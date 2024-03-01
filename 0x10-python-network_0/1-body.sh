53e4e9c05518bc4dd1c55187f1060c0d9a4d7540
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-01 03:23:38 +0300

    Fix 1 and 2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #takes in a URL, sends GET request to URL, and displays body of the response
 curl -sL "$1"
-
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #sends DELETE request to URL passed as the 1st arg & displays the body of the response
 curl -s -X DELETE "$1"
-
