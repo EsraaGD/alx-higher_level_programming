0fe8fc1800aa8a142611f714f5a2f75c7b9d1a08
EsraaGD <isra.hatim2@gmail.com>
2024-02-29 16:21:20 -0800

    T2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #takes in a URL, sends GET request to URL, and displays body of the response
-curl -s -o response.txt -w "%{http_code}" "$1" | awk '/200/{flag=1} flag'
+curl -sL "$1"
+
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-#csends a DELETE request to the URL passed as the first argument and displays the body of the response
+#sends DELETE request to URL passed as the 1st arg & displays the body of the response
+curl -s -X DELETE "$1"
+
